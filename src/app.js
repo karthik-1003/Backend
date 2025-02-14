91b4d31a7f67238d8ab2220f3fe734314d5a18e4
karthik-1003 <karthik.udupa2002@gmail.com>
2025-02-14 11:11:49 +0530

    setup complete

--- src/app.js
+++ src/app.js
@@ -16,4 +16,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public")); //store public assets
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
 export { app };
