75be64a22a615d16ee1f7b4989b9badeb3199792
karthik-1003 <karthik.udupa2002@gmail.com>
2025-02-18 11:48:35 +0530

    Create User

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-dotenv.config({ path: "./env" });
+dotenv.config({ path: "./.env" });
 
 app.on("error", (err) => {
   console.log("ERROR", err);
